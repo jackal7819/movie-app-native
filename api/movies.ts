0afded0944c9420f1f1bb90cdb8642042e7e8442
Viktor Filippov <jackal3166@gmail.com>
2024-04-01 11:32:16 +0300

    Add fetch movies

---
+++ api/movies.ts
@@ -0,0 +1,20 @@
+export const fetchTopRatedMovies = async () => {
+	const url =
+		'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+	const options = {
+		method: 'GET',
+		headers: {
+			accept: 'application/json',
+			Authorization:
+				'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZmU1MTY0ODQzNmFhNmM3Yzc2MGQzMzM0MDI3NzZiOSIsInN1YiI6IjY0YWFjZjkzZmE3OGNkMDBjNTFkODM1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WfgmStP3EGqz8beWEncdqD78KGtEvXaqe_BQXFfCp20',
+		},
+	};
+
+	try {
+		const res = await fetch(url, options);
+		const json = await res.json();
+		return json.results;
+	} catch (error) {
+		console.error('error:' + error);
+	}
+};
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,47 @@
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text } from 'react-native';
 import { View } from '@/components/Themed';
+import { fetchTopRatedMovies } from '@/api/movies';
+import { useEffect, useState } from 'react';
 
 export default function TabOneScreen() {
-	return <View style={styles.container}></View>;
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		const fetchMovies = async () => {
+			setIsLoading(true);
+			const movies = await fetchTopRatedMovies();
+			setMovies(movies);
+			setIsLoading(false);
+		};
+		fetchMovies();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<View style={styles.loader}>
+				<ActivityIndicator size='large' />
+			</View>
+		);
+	}
+
+	return (
+		<View style={styles.container}>
+			<FlatList
+				data={movies}
+				renderItem={({ item }) => <Text>{item.title}</Text>}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
 	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	loader: {
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
